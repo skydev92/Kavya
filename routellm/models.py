8d6bc67d5620999548ac7493adcadbbef79be220
skydev9293 <yourdreamwinner@gmail.com>
2025-01-27 19:14:07 +0100

    Add possiblity to change allowed_html_tags from the request #5

--- routellm/models.py
+++ routellm/models.py
@@ -164,6 +164,7 @@ class ChatCompletionRequest(BaseModel):
     tools: Optional[List[Dict[str, Union[str, int, float]]]] = None
     tool_choice: Optional[str] = None
     user: Optional[str] = None
+    allowed_html_tags: Optional[str] = None
 
 
 class ChatMessage(BaseModel):
